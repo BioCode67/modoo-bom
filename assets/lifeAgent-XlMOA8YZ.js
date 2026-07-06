6d816c0a2b516f99a1e4e96c2bed4fd083f4cc3d
BioCode67 <6wngud@gmail.com>
2026-07-06 14:16:27 +0900

    deploy: 모두봄 프론트엔드 정적 빌드

--- assets/lifeAgent-XlMOA8YZ.js
+++ assets/lifeAgent-XlMOA8YZ.js
@@ -1,4 +1,4 @@
-import{c as m,g as u,s as f}from"./index-DHjE5XWc.js";/**
+import{c as m,g as u,s as f}from"./index-B7Nr_Foy.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
