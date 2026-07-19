a7170ac581bb5f688397fe3aaee988cce2919d1d
BioCode67 <BioCode67@users.noreply.github.com>
2026-07-19 17:44:32 +0000

    deploy: 80bdf1f7ae09972bd3ef4059bb8570878ce79559

--- assets/LocalWelfare-8piznyNu.js
+++ assets/LocalWelfare-8piznyNu.js
@@ -1,4 +1,4 @@
-import{j as e,a as f,m as b}from"./motion-CLPSl4-9.js";import{c as k,k as g,W as N,u as w,S as v,A as y}from"./index-DC1dUTUR.js";/**
+import{j as e,a as f,m as b}from"./motion-CLPSl4-9.js";import{c as k,k as g,W as N,u as w,S as v,A as y}from"./index-CY8wPNjU.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
