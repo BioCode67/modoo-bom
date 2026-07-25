ccb55128becfcde5f0f9a81c891fccb8e4f05073
BioCode67 <BioCode67@users.noreply.github.com>
2026-07-25 17:57:23 +0000

    deploy: bc049bbec3737666b2f040cf5f92d3dd06fe2a65

--- assets/LocalWelfare-4Y_3bfWW.js
+++ assets/LocalWelfare-4Y_3bfWW.js
@@ -1,4 +1,4 @@
-import{j as e,a as f,m as b}from"./motion-CLPSl4-9.js";import{c as k,e as g,z as N,u as w,S as v,A as y}from"./index-CwMIP16U.js";/**
+import{j as e,a as f,m as b}from"./motion-CLPSl4-9.js";import{c as k,e as g,z as N,u as w,S as v,A as y}from"./index-BQbuYGSR.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
